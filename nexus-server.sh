9f4b097a197e04ddecc9e4e90d4ea494802b0d38
rknikhade1419 <nikhaderoshankumar@gmail.com>
2026-02-17 10:35:05 +0530

    Update nexus-server.sh

--- nexus-server.sh
+++ nexus-server.sh
@@ -1,37 +1,10 @@
-/* #!/bin/bash  
-
-###############################################################################################################
-#                   Docker Installation
-###############################################################################################################
-
-# Add Docker's official GPG key:
-# apt update -y
-# sudo apt-get install -y ca-certificates curl gnupg
-# sudo install -m 0755 -d /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-# sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# # Add the repository to Apt sources:
-# echo \
-#   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt-get update -y
-
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-apt update -y
-apt install -y docker.io 
-
-docker run -d -p 8081:8081 --name nexus-server sonatype/nexus3 */
-
 #!/bin/bash
-# ... (Your Docker install steps) ...
 apt update -y
 apt install -y docker.io 
-# CRITICAL: Increase Virtual Memory for Elasticsearch
-sysctl -w vm.max_map_count=262144
-echo "vm.max_map_count=262144" >> /etc/sysctl.conf
 
-# Run SonarQube
+# Create volume for persistence
+docker volume create nexus-data
 
-docker run -d -p 8081:8081 -v nexus-data:/nexus-data sonatype/nexus3
+# Run Nexus (Port 8081)
+# Mapping internal /nexus-data to the docker volume
+docker run -d -p 8081:8081 -v nexus-data:/nexus-data --name nexus-server sonatype/nexus3
